138505f271c2c19e806d874b641caf6128a2a51e
matias <matifandy@gmail.com>
2022-11-12 18:47:16 -0300

    add styles with tailwind

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
-import styles from '../styles/Home.module.css'
+import '../styles/globals.css';
+
 
 
 export default function RootLayout({
@@ -9,11 +10,34 @@ export default function RootLayout({
   return (
     <html>
       <head></head>
-      <body>
+      <body className="flex flex-col h-screen">
+        <nav className="relative w-full flex flex-wrap items-center justify-between py-3 bg-gray-100 text-gray-500 hover:text-gray-700 focus:text-gray-700 shadow-lg">
+          <div className="container-fluid w-full flex flex-wrap items-center justify-between px-6">
+            <div className="container-fluid">
+              <a className="text-xl text-black" href="#">Random Interview Question</a>
+            </div>
+          </div>
+        </nav>
         {children}
-        <footer className={styles.footer}>
-          Made by Matias Fandiño
-      </footer>
+        <footer className="mt-auto p-4 bg-white rounded-lg shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800">
+          <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 <a href="https://github.com/matifandy8" className="hover:underline">Matias Fandiño™</a>. All Rights Reserved.
+          </span>
+          <ul className="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
+            <li>
+              <a href="#" className="mr-4 hover:underline md:mr-6 ">About</a>
+            </li>
+            <li>
+              <a href="#" className="mr-4 hover:underline md:mr-6">Privacy Policy</a>
+            </li>
+            <li>
+              <a href="#" className="mr-4 hover:underline md:mr-6">Licensing</a>
+            </li>
+            <li>
+              <a href="#" className="hover:underline">Contact</a>
+            </li>
+          </ul>
+        </footer>
+
       </body>
     </html>
   )
